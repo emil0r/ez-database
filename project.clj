efb3fcd8ecd15acd566c2e8be69e9d46d5d1f8fe
Emil Bengtsson <emil@emil0r.com>
2018-10-22 14:24:13 +0200

    0.7.0-alpha2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ez-database "0.7.0-SNAPSHOT"
+(defproject ez-database "0.7.0-alpha2"
   :description "Handling database queries with ease"
   :url "https://github.com/emil0r/ez-database"
   :license {:name "Eclipse Public License"
